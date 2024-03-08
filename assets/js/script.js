8995118759204b6911a1ca059918cfda4513be21
cfurrutia <ing.cfurrutia@gmail.com>
2024-03-08 20:55:41 -0300

    se agrega boton de mensaje enviado y detalles

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,15 @@ var nav = document.querySelector('.navbar');
 
 // desplazamiento
 window.addEventListener('scroll', function () {
-    if (window.scrollY > 100) {
+    if (window.scrollY > 540) {
         nav.classList.add('bg-dark', 'shadow');
     } else {
         nav.classList.remove('bg-dark', 'shadow');
     }
+});
+
+$(function () {
+    $("#enviarMensaje").click(function () {
+        alert("El mensaje fue enviado correctamente...")
+    });
 });
