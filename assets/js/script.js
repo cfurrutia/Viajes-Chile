bf28ad01b88d96f36aacef5b5162f08a8c1d53e8
cfurrutia <ing.cfurrutia@gmail.com>
2024-03-09 19:04:17 -0300

    agregando boton minimizado y detalles visuales

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     // navbar
     var nav = document.querySelector('.navbar');
     window.addEventListener('scroll', function () {
-        if (window.scrollY > 540) {
+        if (window.scrollY > 800) {
             nav.classList.add('bg-dark', 'shadow');
         } else {
             nav.classList.remove('bg-dark', 'shadow');
