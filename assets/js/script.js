66537a00cf7e01fb20cfbc952276e6281dfcc97e
cfurrutia <ing.cfurrutia@gmail.com>
2024-03-08 22:58:23 -0300

    se agrega smooth scroll y otras funciones

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,37 @@
-// navbar
-var nav = document.querySelector('.navbar');
+$(document).ready(function () {
 
-// desplazamiento
-window.addEventListener('scroll', function () {
-    if (window.scrollY > 540) {
-        nav.classList.add('bg-dark', 'shadow');
-    } else {
-        nav.classList.remove('bg-dark', 'shadow');
-    }
-});
 
-$(function () {
-    $("#enviarMensaje").click(function () {
-        alert("El mensaje fue enviado correctamente...")
+    // navbar
+    var nav = document.querySelector('.navbar');
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 540) {
+            nav.classList.add('bg-dark', 'shadow');
+        } else {
+            nav.classList.remove('bg-dark', 'shadow');
+        }
     });
+
+    // smooth scroll
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const target = document.querySelector(this.getAttribute('href'));
+            target.scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+    // destacados - al hacer click en el titulo desaparace el texto
+    $(".card-title").click(function () {
+        $(".card-text").toggle();
+    });
+
+    // Formulario enviar mensaje de contacto
+    $(function () {
+        $("#enviarMensaje").click(function () {
+            alert("El mensaje fue enviado correctamente...")
+        });
+    });
+
 });
